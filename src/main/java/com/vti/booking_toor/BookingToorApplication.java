a3a4f45070528a966e9e49a1ca4341530a074149
danhcat <121871520+danhcat@users.noreply.github.com>
2023-08-29 20:33:17 +0700

    sumary
    push

---
+++ src/main/java/com/vti/booking_toor/BookingToorApplication.java
@@ -0,0 +1,13 @@
+package com.vti.booking_toor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingToorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingToorApplication.class, args);
+	}
+
+}
